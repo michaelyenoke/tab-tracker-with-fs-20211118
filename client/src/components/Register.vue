d5eca9135015e525b7a8e921c23290441494dee3
funkoke <michaelyenoke@gmail.com>
2021-11-20 20:33:47 +0800

    update ui register button by vue

---
+++ client/src/components/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>Register</h1>
+    <input
+          type='email'
+          text='email'
+          v-model='email'
+          placeholder='email' />
+    <br>
+    <input
+          type='password'
+          text='password'
+          v-model='password'
+          placeholder='password' />
+    <br>
+    <button
+      @click='register'>
+      Register
+    </button>
+
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '123',
+      password: 'abc'
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
