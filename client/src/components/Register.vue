2ba80f2b712980b1b50cd6546d6c4d1c0b0cd4a7
funkoke <michaelyenoke@gmail.com>
2021-11-24 18:23:05 +0800

    update

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -13,6 +13,8 @@
           v-model='password'
           placeholder='password' />
     <br>
+    <div class='error' v-html='error' />
+    <br>
     <button
       @click='register'>
       Register
@@ -27,16 +29,21 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        var response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+        console.log(response.data)
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -44,4 +51,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  color:red;
+}
 </style>
